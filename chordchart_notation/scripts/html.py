8661927a8c0794dc09e15ef018d95cc7cd7984bd
Antoine Cezar <antoine@cezar.fr>
2017-12-01 02:01:05 +0100

    Add mutiple files support to chordchart-to-html

--- chordchart_notation/scripts/html.py
+++ chordchart_notation/scripts/html.py
@@ -13,10 +13,12 @@ def parse_args(argv=sys.argv):
         Render chordchart notation to html.
         ''')
 
-    parser.add_argument('input', metavar='FILE', type=str, help='''
-        A file to render.
-        If no files provided, it reads from stdin.
-        ''')
+    parser.add_argument('input', metavar='FILE', type=str,
+                        nargs='+',
+                        help='''
+                            A file to render.
+                            If no files provided, it reads from stdin.
+                        ''')
 
     return parser.parse_args(argv[1:])
 
@@ -29,18 +31,24 @@ def render(text):
     return builder.get_result()
 
 
+def render_file(path):
+    with open(path, 'r') as fd:
+        text = fd.read()
+
+    output = os.path.splitext(path)[0] + '.html'
+    out = open(output, 'w')
+
+    result = render(text)
+    out.write(result)
+
+
 def main():
     args = parse_args()
 
     if not args.input:
         text = sys.stdin.read()
-        out = sys.stdout
+        result = render(text)
+        sys.stdout.write(result)
     else:
-        with open(args.input, 'r') as fd:
-            text = fd.read()
-
-        output = os.path.splitext(args.input)[0] + '.html'
-        out = open(output, 'w')
-
-    result = render(text)
-    out.write(result)
+        for filename in args.input:
+            render_file(filename)
